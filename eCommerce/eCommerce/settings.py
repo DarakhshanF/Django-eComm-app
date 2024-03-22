ade53344a448578ffdd208e137a0f4d1362a395b
Darakhshan Fazail <darakhshanfazail@sarfarazs-air.wkstn.qmul.ac.uk>
2024-03-22 14:37:26 +0000

    Create CustomUser and CustomUserManager for 'users' app

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -82,6 +82,8 @@ DATABASES = {
     }
 }
 
+# Custom user model for authentication
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
