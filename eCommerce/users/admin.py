c207cce1e5aceb9feff62c377440abf695c7395e
Darakhshan Fazail <darakhshanfazail@sarfarazs-air.wkstn.qmul.ac.uk>
2024-03-22 14:44:10 +0000

    Regsiter CustomUser to Admin panel

--- eCommerce/users/admin.py
+++ eCommerce/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
+admin.site.register(CustomUser)
