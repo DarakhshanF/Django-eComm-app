5ada2a4f2e15c42a53c24e5747dd8b1037cc1045
Darakhshan Fazail <darakhshanfazail@sarfarazs-air.wkstn.qmul.ac.uk>
2024-03-22 14:56:45 +0000

    Update the layout and fields of CustomUser on admin panel

--- eCommerce/users/admin.py
+++ eCommerce/users/admin.py
@@ -4,4 +4,22 @@ from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    # _ allowing the string to be translated into different languages
+    fieldsets = (
+        (_('User Details'), {'fields': ('email', 'password')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
