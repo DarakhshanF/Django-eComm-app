88ed006fb7ce37724901b8db428a8bc4a4d90b66
Darakhshan Fazail <darakhshanfazail@sarfarazs-air.wkstn.qmul.ac.uk>
2024-03-27 16:36:16 +0000

    Add 'home' app and add index.html as base for the header and footer sections

--- eCommerce/users/urls.py
+++ eCommerce/users/urls.py
@@ -5,7 +5,7 @@ app_name = "users"
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path("register/", views.registration.as_view(), name="register"),
+    path('register/', views.registration.as_view(), name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
 ]
