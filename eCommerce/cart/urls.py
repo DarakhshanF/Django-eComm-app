f1b439afefec24e90f871e68feea53f0dde127b0
DarakhshanF <fazailshahid@gmail.com>
2024-05-01 13:49:00 +0100

    Remove commented-out urls from the urls.py file

--- eCommerce/cart/urls.py
+++ eCommerce/cart/urls.py
@@ -5,10 +5,7 @@ app_name = "cart"
 
 urlpatterns = [
     path('', views.cart, name='cart_detail'),
-    # path('', views.cart_detail, name='cart_detail'),
-    # path('register/', views.registration.as_view(), name='register'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    # path('change/<int:item_id>/<int:quantity>/', views.change_item_quantity, name='change_item_quantity'),
     path('change/<int:item_id>/', views.change_item_quantity, name='change_item_quantity'),
 ]
