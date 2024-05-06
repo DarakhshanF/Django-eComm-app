dae258d956d9defa01da65d97577bace04f18614
EC2 Default User <ec2-user@ip-172-31-39-123.eu-west-2.compute.internal>
2024-05-06 19:13:35 +0000

    Set Debug to False and Update allowed host

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i+7pxxp#8#jr7swa#at$iv4%lfvb*manvcs__&+a^f80)44gmz"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-13-42-43-198.eu-west-2.compute.amazonaws.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
